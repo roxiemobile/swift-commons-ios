9b1c02c9156b744d153810bc7be57d4fd9864c6d
Nikita Semakov <SemakovNV@ekassir.com>
2016-12-06 20:39:22 +0300

    Add tests for array validation and required model fields validation

--- modules/swift-commons/SwiftCommonsTests/Sources/SwiftCommonsTests.swift
+++ modules/swift-commons/SwiftCommonsTests/Sources/SwiftCommonsTests.swift
@@ -25,19 +25,102 @@ class SwiftCommonsTests: XCTestCase
         // Put teardown code here. This method is called after the invocation of each test method in the class.
         super.tearDown()
     }
-    
-    func testExample() {
-        // This is an example of a functional test case.
-        XCTAssert(true, "Pass")
+
+    func testModelWithoutRequiredField()
+    {
+        let model = VehicleModelTest(params: [JsonKeys.Model : "KAMAZ"])
+
+        XCTAssertNil(model, "model should be nil")
     }
-    
+
+    func testModelWithRequiredFields()
+    {
+        let model = VehicleModelTest(params: [
+            JsonKeys.Model : "KAMAZ",
+            JsonKeys.Color : "Black"])
+
+        XCTAssertNotNil(model, "model shouldn't be nil")
+    }
+
+    func testModelWithRequiredArrayFieldWithNonValidModel()
+    {
+        if let json = getJSON("ParkingModelWithOneNonValidVehicleInArray")
+        {
+            let parking = ParkingModelTest(params:json)
+            XCTAssertNil(parking, "model should be nil")
+        }
+        else {
+            fatalError("no such file")
+        }
+    }
+
+    func testModelWithRequiredArrayFieldWithValidModel()
+    {
+        if let json = getJSON("ParkingModelWithValidVehiclesInArray")
+        {
+            let parking = ParkingModelTest(params:json)
+            XCTAssertNotNil(parking, "model should be non nil")
+        }
+        else {
+            fatalError("no such file")
+        }
+    }
+
+    func testModelWithOptionalArrayFieldWithNonValidModel()
+    {
+        if let json = getJSON("ParkingModelWithOneNonValidVehicleInArray")
+        {
+            let parking = ParkingModelWithOptionalVehicles(params:json)
+            XCTAssertNil(parking, "model should be nil")
+        }
+        else {
+            fatalError("no such file")
+        }
+    }
+
+    func testModelWithOptionalArrayFieldWithValidModel()
+    {
+        if let json = getJSON("ParkingModelWithValidVehiclesInArray")
+        {
+            let parking = ParkingModelWithOptionalVehicles(params:json)
+            XCTAssertNotNil(parking, "model should be non nil")
+        }
+        else {
+            fatalError("no such file")
+        }
+    }
+
     func testPerformanceExample() {
         // This is an example of a performance test case.
         self.measureBlock() {
             // Put the code you want to measure the time of here.
         }
     }
-    
+
+    func getJSON(fileName: String) -> [String : AnyObject]?
+    {
+        if let filePath = NSBundle(forClass: self.dynamicType).pathForResource(fileName, ofType: "json") {
+            do {
+                let data = NSData(contentsOfFile: filePath)
+                if let data = data {
+                    let json = try NSJSONSerialization.JSONObjectWithData(data, options: NSJSONReadingOptions.AllowFragments)
+
+                    return json as? [String : AnyObject]
+                }
+            }
+            catch let error as NSError {
+                print("error loading contentsOf url \(filePath)")
+                print(error.localizedDescription)
+                return nil
+            }
+        }
+        else {
+            return nil
+        }
+
+        return nil
+    }
+
 }
 
 // ----------------------------------------------------------------------------
