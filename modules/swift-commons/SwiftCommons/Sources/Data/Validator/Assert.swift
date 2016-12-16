5fc7f915cf485279e644e88d6b8ba3fd14408eed
Nikita Semakov <SemakovNV@ekassir.com>
2016-12-16 17:37:56 +0300

    Line number and file names were added. Number of wrong argument was placed in message. Change class to become NonCreatable successor.

--- modules/swift-commons/SwiftCommons/Sources/Data/Validator/Assert.swift
+++ modules/swift-commons/SwiftCommons/Sources/Data/Validator/Assert.swift
@@ -12,104 +12,106 @@ import Foundation
 
 // ----------------------------------------------------------------------------
 
-public final class Assert
+public final class Assert: NonCreatable
 {
 // MARK: - Functions
 
-    public static func isTrue(condition: Bool, message: String?) throws
+    public static func isTrue(condition: Bool, message: String?, line: UInt? = #line, file: String? = #file) throws
     {
         if !(condition) {
-            try fail(message)
+            try fail(message, line: line, file: file)
         }
     }
 
-    public static func isTrue(condition: Bool) throws {
-        try isTrue(condition, message: nil)
+    public static func isTrue(condition: Bool, line: UInt? = #line, file: String? = #file) throws {
+        try isTrue(condition, message: nil, line: line, file: file)
     }
 
-    public static func isFalse(condition: Bool, message: String?) throws {
-        try isTrue(!condition, message: message)
+    public static func isFalse(condition: Bool, message: String?, line: UInt? = #line, file: String? = #file) throws {
+        try isTrue(!condition, message: message, line: line, file: file)
     }
 
-    public static func isFalse(condition: Bool) throws {
-        try isTrue(!condition)
+    public static func isFalse(condition: Bool, line: UInt? = #line, file: String? = #file) throws {
+        try isTrue(!condition, line: line, file: file)
     }
 
-    public static func fail(message: String?) throws
+    public static func fail(message: String?, line: UInt? = #line, file: String? = #file) throws
     {
         if let message = message {
-            throw AssertionError(message: message)
+            throw AssertionError(message: message, line: line, file: file)
         }
-        throw AssertionError(message: nil)
+        throw AssertionError(message: nil, line: line, file: file)
     }
 
-    public static func fail() throws {
-        try fail(nil)
+    public static func fail(line: UInt? = #line, file: String? = #file) throws {
+        try fail(nil, line: line, file: file)
     }
 
-    public static func isEquals<T: Equatable>(expected: T?, actual: T?, message: String?) throws
+    public static func isEquals<T: Equatable>(expected: T?, actual: T?, message: String?, line: UInt? = #line, file: String? = #file) throws
     {
         if !equalsRegardingNil(expected, actual: actual) {
-            throw AssertionError(message: message)
+            throw AssertionError(message: message, line: line, file: file)
         }
     }
 
-    public static func isNotEquals<T: Equatable>(unexpected: T?, actual: T?, message: String?) throws
+    public static func isNotEquals<T: Equatable>(unexpected: T?, actual: T?, message: String?, line: UInt? = #line, file: String? = #file) throws
     {
         if equalsRegardingNil(unexpected, actual: actual) {
-            throw AssertionError(message: message)
+            throw AssertionError(message: message, line: line, file: file)
         }
     }
 
-    public static func isNotNil(object: AnyObject?, message: String?) throws {
-        try isTrue(object != nil, message: message)
+    public static func isNotNil(object: AnyObject?, message: String?, line: UInt? = #line, file: String? = #file) throws {
+        try isTrue(object != nil, message: message, line: line, file: file)
     }
 
-    public static func isNil(object: AnyObject?, message: String?) throws {
-        try isTrue(object == nil, message: message)
+    public static func isNil(object: AnyObject?, message: String?, line: UInt? = #line, file: String? = #file) throws {
+        try isTrue(object == nil, message: message, line: line, file: file)
     }
 
-    public static func isValid(array: ParcelableModel? ...) throws
+    public static func isValid(array: ParcelableModel? ..., line: UInt? = #line, file: String? = #file) throws
     {
-        for obj in array
+        for (index, obj) in array.enumerate()
         {
-            guard let obj = obj else { throw AssertionError(message: "Object cannot be nil") }
+            guard let obj = obj else { throw AssertionError(message: "Object[\(index)] cannot be nil", line: line, file: file) }
             try obj.validate()
         }
     }
     
-    public static func isValid(array: [ParcelableModel]? ...) throws
+    public static func isValid(array: [ParcelableModel]? ..., line: UInt? = #line, file: String? = #file) throws
     {
-        for arr in array
+        for (index, arr) in array.enumerate()
         {
-            guard let arr = arr else { throw AssertionError(message: "Object cannot be nil") }
+            guard let arr = arr else { throw AssertionError(message: "Object[\(index)] cannot be nil", line: line, file: file) }
             for obj in arr {
                 try obj.validate()
             }
         }
     }
     
-    public static func isValid(array: [ParcelableModel?]? ...) throws
+    public static func isValid(array: [ParcelableModel?]? ..., line: UInt? = #line, file: String? = #file) throws
     {
-        for arr in array
+        for (index, arr) in array.enumerate()
         {
-            guard let arr = arr else { throw AssertionError(message: "Object cannot be nil") }
-            for obj in arr {
-                guard let obj = obj else { throw AssertionError(message: "Object cannot be nil") }
+            guard let arr = arr else { throw AssertionError(message: "Object[\(index)] cannot be nil", line: line, file: file) }
+            for (index2, obj) in arr.enumerate()
+            {
+                guard let obj = obj else { throw AssertionError(message: "Object[\(index), \(index2)] cannot be nil", line: line, file: file) }
                 try obj.validate()
             }
         }
     }
 
-    public static func isNilOrValid(array: ParcelableModel? ...) throws
+    public static func isNilOrValid(array: ParcelableModel? ..., line: UInt? = #line, file: String? = #file) throws
     {
-        for obj in array {
+        for obj in array
+        {
             guard let obj = obj else { return }
             try obj.validate()
         }
     }
     
-    public static func isNilOrValid(array: [ParcelableModel]? ...) throws
+    public static func isNilOrValid(array: [ParcelableModel]? ..., line: UInt? = #line, file: String? = #file) throws
     {
         for arr in array
         {
@@ -120,70 +122,61 @@ public final class Assert
         }
     }
     
-    public static func isNilOrValid(array: [ParcelableModel?]? ...) throws
+    public static func isNilOrValid(array: [ParcelableModel?]? ..., line: UInt? = #line, file: String? = #file) throws
     {
-        for arr in array
+        for (index, arr) in array.enumerate()
         {
             guard let arr = arr else { return }
-            for obj in arr {
-                guard let obj = obj else { throw AssertionError(message: "Object cannot be nil") }
+            for (index2, obj) in arr.enumerate()
+            {
+                guard let obj = obj else { throw AssertionError(message: "Object[\(index), \(index2)] cannot be nil", line: line, file: file) }
                 try obj.validate()
             }
         }
     }
 
-    public static func isNotEmpty(array: String? ...) throws
+    public static func isNotEmpty(array: String? ..., line: UInt? = #line, file: String? = #file) throws
     {
-        for obj in array
+        for (index, obj) in array.enumerate()
         {
-            guard let obj = obj where !(obj.isEmpty) else { throw AssertionError(message: "String cannot be empty") }
+            guard let obj = obj where !(obj.isEmpty) else { throw AssertionError(message: "String[\(index)] cannot be empty", line: line, file: file) }
         }
     }
 
-    public static func isNotEmpty(array: [String]? ...) throws
+    public static func isNotEmpty(array: [String]? ..., line: UInt? = #line, file: String? = #file) throws
     {
-        for arr in array
+        for (index, arr) in array.enumerate()
         {
-            guard let arr = arr else { throw AssertionError(message: "Object cannot be nil") }
-            for obj in arr {
+            guard let arr = arr else { throw AssertionError(message: "Object[\(index)] cannot be nil", line: line, file: file) }
+            for (index2, obj) in arr.enumerate()
+            {
                 if obj.isEmpty {
-                    throw AssertionError(message: "String cannot be empty")
+                    throw AssertionError(message: "String[\(index), \(index2)] cannot be empty", line: line, file: file)
                 }
             }
         }
     }
 
-    public static func isNotEmpty(array: [String?]? ...) throws
+    public static func isNotEmpty(array: [String?]? ..., line: UInt? = #line, file: String? = #file) throws
     {
-        for arr in array
+        for (index, arr) in array.enumerate()
         {
-            guard let arr = arr else { throw AssertionError(message: "Object cannot be nil") }
-            for obj in arr {
-                guard let obj = obj where !(obj.isEmpty) else { throw AssertionError(message: "Object cannot be nil") }
+            guard let arr = arr else { throw AssertionError(message: "Object[\(index)] cannot be nil", line: line, file: file) }
+            for (index2, obj) in arr.enumerate()
+            {
+                guard let obj = obj else { throw AssertionError(message: "Object[\(index), \(index2)] cannot be nil", line: line, file: file) }
+                if obj.isEmpty {
+                    throw AssertionError(message: "Object[\(index), \(index2)] cannot be empty", line: line, file: file)
+                }
             }
         }
     }
 
-    public static func isNotEmptyAtLeastOne(array: [String?]) throws
+    public static func isEmpty(array: [String?]? ..., line: UInt? = #line, file: String? = #file) throws
     {
-        var result = false
-        for obj in array
+        for (index, obj) in array.enumerate()
         {
-            if let obj = obj where !(obj.isEmpty) {
-                result = true
-                return
-            }
-        }
-        if !result {
-            throw AssertionError(message: "Array should have at least one non empty member")
-        }
-    }
-
-    public static func isEmpty(array: [String]? ...) throws
-    {
-        for obj in array
-        {
-            guard let obj = obj where (obj.isEmpty) else { throw AssertionError(message: "String cannot be empty") }
+            guard let obj = obj where (obj.isEmpty) else { throw AssertionError(message: "String[\(index)] cannot be empty", line: line, file: file) }
         }
     }
 
