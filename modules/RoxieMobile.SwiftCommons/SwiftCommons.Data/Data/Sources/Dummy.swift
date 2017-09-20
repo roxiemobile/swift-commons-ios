e74353725c27c1b485db95fb63400b35befc1920
Alexander Bragin <alexander.bragin@gmail.com>
2017-09-20 21:34:44 +0300

    Added projects ‘SwiftCommons.Data’, ‘SwiftCommons.Diagnostics’.

---
+++ modules/RoxieMobile.SwiftCommons/SwiftCommons.Data/Data/Sources/Dummy.swift
@@ -0,0 +1,21 @@
+// ----------------------------------------------------------------------------
+//
+//  Dummy.swift
+//
+//  @author     Alexander Bragin <bragin-av@roxiemobile.com>
+//  @copyright  Copyright (c) 2017, Roxie Mobile Ltd. All rights reserved.
+//  @link       http://www.roxiemobile.com/
+//
+// ----------------------------------------------------------------------------
+
+import Foundation
+
+// ----------------------------------------------------------------------------
+
+struct Dummy {
+    static func sayHello() {
+        print("Hello from Dummy!")
+    }
+}
+
+// ----------------------------------------------------------------------------
