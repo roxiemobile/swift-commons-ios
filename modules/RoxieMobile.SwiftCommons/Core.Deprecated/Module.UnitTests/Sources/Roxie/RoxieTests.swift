356bfebc0e4b3b53e393fb3414580ee54cda7c60
Alexander Bragin <alexander.bragin@gmail.com>
2017-10-05 03:25:19 +0300

    Fixed dependencies of ‘SwiftCommons.Core.Deprecated.UnitTests’ module.

--- modules/RoxieMobile.SwiftCommons/Core.Deprecated/Module.UnitTests/Sources/Roxie/RoxieTests.swift
+++ modules/RoxieMobile.SwiftCommons/Core.Deprecated/Module.UnitTests/Sources/Roxie/RoxieTests.swift
@@ -9,6 +9,7 @@
 // ----------------------------------------------------------------------------
 
 @testable import SwiftCommonsCoreDeprecated
+import SwiftCommons
 import XCTest
 
 // ----------------------------------------------------------------------------
@@ -18,18 +19,22 @@ class RoxieTests: XCTestCase
 // MARK: - Tests
 
     func testExpectationError() {
-        var name: String?
-
-        name = Roxie.typeName(of: ExpectationError(""))
+        var name = Roxie.typeName(of: ExpectationError(""))
         XCTAssertEqual(name, "ExpectationError")
+    }
 
-        name = Roxie.typeName(of: ObjectMapperException.self)
+    func testObjectMapperException() {
+        var name = Roxie.typeName(of: ObjectMapperException.self)
         XCTAssertEqual(name, "ObjectMapperException")
+    }
 
-        name = Roxie.typeName(of: DatabaseHelper(databaseName: "", version: 0))
+    func testDatabaseHelper() {
+        var name = Roxie.typeName(of: DatabaseHelper(databaseName: "", version: 0))
         XCTAssertEqual(name, "DatabaseHelper")
+    }
 
-// FIXME: @see ValidatableModel.swift:157
+    func testValidatableModel() {
+//        // FIXME: @see ValidatableModel.swift:157
 //        name = Roxie.typeName(of: ValidatableModel(params: [String:Any]()))
 //        XCTAssertEqual(name, "ValidatableModel")
     }
