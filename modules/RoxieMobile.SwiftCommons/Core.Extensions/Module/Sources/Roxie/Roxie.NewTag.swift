f86c7b793c47d945a4e3329405737fa8ed3124a1
Alexander Bragin <alexander.bragin@gmail.com>
2017-10-25 14:14:31 +0300

    Bug fix in Roxie.newTag()

--- modules/RoxieMobile.SwiftCommons/Core.Extensions/Module/Sources/Roxie/Roxie.NewTag.swift
+++ modules/RoxieMobile.SwiftCommons/Core.Extensions/Module/Sources/Roxie/Roxie.NewTag.swift
@@ -20,8 +20,8 @@ public extension Roxie
     /// Creates a new URN tag for any object.
     public static func newTag(for subject: Any) -> String
     {
-        let nameTag = String(format:"%08lx", UInt32(Roxie.typeName(of: subject).hashValue) & 0xFFFFFFFF)
-        let timeTag = String(format:"%08lx", UInt32(mach_absolute_time()) & 0xFFFFFFFF)
+        let nameTag = String(format:"%08lx", UInt32(truncatingIfNeeded: Roxie.typeName(of: subject).hashValue))
+        let timeTag = String(format:"%08lx", UInt32(truncatingIfNeeded: mach_absolute_time()))
 
         // Done
         return "urn:tag:\(nameTag):\(timeTag)"
