d8f970ad943f7f34d96967f74d75a6c3fb6409b6
Мамунина Наталья Вадимовна <MamuninaNV@ekassir.com>
2018-03-05 18:13:38 +0300

    Divided Integer Operators to Signed and Unsigned. Divided Integer Operators to OptionalType, ImplicityUnwrappedOptionalType, Type.

---
+++ Modules/RoxieMobile.SwiftCommons/Tests/Data.UnitTests/Sources/IntegerOperators/SignedInteger/IntegerOperators.ImplicityUnwrappedOptionalSignedIntegerMapping.swift
@@ -0,0 +1,51 @@
+// ----------------------------------------------------------------------------
+//
+//  IntegerOperators.swift
+//
+//  @author     Natalia Mamunina <___EMAIL___>
+//  @copyright  Copyright (c) 2018, ___COMPANY___. All rights reserved.
+//  @link       ___WEBSITE___
+//
+// ----------------------------------------------------------------------------
+
+import Foundation
+
+// ----------------------------------------------------------------------------
+
+class IntegerOperators: NSObject
+{
+// MARK: - Construction
+
+    // ...
+
+// MARK: - Properties
+
+    // ...
+
+// MARK: - Methods
+
+    // ...
+
+// MARK: - Actions
+
+    // ...
+
+// MARK: - Private Methods
+
+    // ...
+
+// MARK: - Inner Types
+
+    // ...
+
+// MARK: - Constants
+
+    // ...
+
+// MARK: - Variables
+
+    // ...
+
+}
+
+// ----------------------------------------------------------------------------
---
+++ Modules/RoxieMobile.SwiftCommons/Tests/Data.UnitTests/Sources/IntegerOperators/SignedInteger/IntegerOperators.SignedIntegerMapping.swift
@@ -0,0 +1,51 @@
+// ----------------------------------------------------------------------------
+//
+//  IntegerOperators.SignedInteger.swift
+//
+//  @author     Natalia Mamunina <___EMAIL___>
+//  @copyright  Copyright (c) 2018, ___COMPANY___. All rights reserved.
+//  @link       ___WEBSITE___
+//
+// ----------------------------------------------------------------------------
+
+import Foundation
+
+// ----------------------------------------------------------------------------
+
+class IntegerOperators_SignedInteger: NSObject
+{
+// MARK: - Construction
+
+    // ...
+
+// MARK: - Properties
+
+    // ...
+
+// MARK: - Methods
+
+    // ...
+
+// MARK: - Actions
+
+    // ...
+
+// MARK: - Private Methods
+
+    // ...
+
+// MARK: - Inner Types
+
+    // ...
+
+// MARK: - Constants
+
+    // ...
+
+// MARK: - Variables
+
+    // ...
+
+}
+
+// ----------------------------------------------------------------------------
