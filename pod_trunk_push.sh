06f0400b052ec565eb503438ad6ff21aa5f61841
Alexander Bragin <alexander.bragin@gmail.com>
2018-10-14 01:50:24 +0300

    Added script for sending libraries to the Specs repo.

---
+++ pod_trunk_push.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+pod trunk push SwiftCommonsAbstractions.podspec
+pod trunk push SwiftCommonsObjC.podspec
+pod trunk push SwiftCommonsConcurrent.podspec
+pod trunk push SwiftCommonsLang.podspec
+pod trunk push SwiftCommonsLogging.podspec
+pod trunk push SwiftCommonsExtensions.podspec
+pod trunk push SwiftCommonsDiagnostics.podspec
+pod trunk push SwiftCommonsData.podspec
+pod trunk push SwiftCommons.podspec
