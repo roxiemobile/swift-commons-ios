39ff31b3e9bc69b385b6c8a92fadd99f79b7376d
Alexander Bragin <alexander.bragin@gmail.com>
2019-02-02 14:40:57 +0300

    Merged changes from ObjectMapper v3.4.2

--- Modules/RoxieMobile.SwiftCommons/Sources/Data/Dependencies/ObjectMapper/Transforms/CodableTransform.swift
+++ Modules/RoxieMobile.SwiftCommons/Sources/Data/Dependencies/ObjectMapper/Transforms/CodableTransform.swift
@@ -6,7 +6,7 @@
 //
 //  The MIT License (MIT)
 //
-//  Copyright (c) 2014-2016 Hearst
+//  Copyright (c) 2014-2018 Tristan Himmelman
 //
 //  Permission is hereby granted, free of charge, to any person obtaining a copy
 //  of this software and associated documentation files (the "Software"), to deal
@@ -56,7 +56,7 @@ open class CodableTransform<T: Codable>: TransformType {
         do {
             let encoder = JSONEncoder()
             let data = try encoder.encode(item)
-            let dictionary = try JSONSerialization.jsonObject(with: data, options: .allowFragments) as? [String: Any]
+            let dictionary = try JSONSerialization.jsonObject(with: data, options: .allowFragments)
             return dictionary
         } catch {
             return nil
