3dbae42080b48c784c6b696ad0ab1fe9339ab8b0
Komarov Evgeniy <KomarovEV@ekassir.com>
2019-06-28 14:35:15 +0300

    RMCMT-114 Add SwiftLint to each containing project

---
+++ Modules/RoxieMobile.SwiftCommons/Resources/Scripts/SwiftLint.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+#  @author     Komarov Evgeniy <KomarovEV@ekassir.com>
+#  @copyright  Copyright (c) 2019, Roxie Mobile Ltd. All rights reserved.
+#  @link       http://www.roxiemobile.ru/
+#
+# ---
+
+"${PODS_ROOT}/SwiftLint/swiftlint"
+
+# ---
