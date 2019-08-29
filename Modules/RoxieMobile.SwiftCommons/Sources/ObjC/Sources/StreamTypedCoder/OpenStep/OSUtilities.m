a78d4b3813e62169e9d3c9a7084354a84ba331e7
Alexander Bragin <alexander.bragin@gmail.com>
2019-08-30 02:39:30 +0300

    WIP (rev.17)

--- Modules/RoxieMobile.SwiftCommons/Sources/ObjC/Sources/StreamTypedCoder/OpenStep/OSUtilities.m
+++ Modules/RoxieMobile.SwiftCommons/Sources/ObjC/Sources/StreamTypedCoder/OpenStep/OSUtilities.m
@@ -1,5 +1,5 @@
 /* 
-   NSUtilities.m
+   OSUtilities.m
 
    Copyright (C) 1995, 1996 Ovidiu Predescu and Mircea Oancea.
    All rights reserved.
@@ -23,129 +23,93 @@
    or in connection with the use or performance of this software.
 */
 
-//#include <Foundation/common.h>
-#include <Foundation/NSString.h>
-//#include <Foundation/NSUtilities.h>
-//#include <Foundation/NSDate.h>
-//#include <Foundation/NSProcessInfo.h>
-//#include <Foundation/NSPosixFileDescriptor.h>
-//#include <Foundation/NSLock.h>
-//#include <Foundation/NSByteOrder.h>
-//#include <config.h>
-
+// #include <Foundation/common.h>
 #include "common.h"
+#include <Foundation/NSString.h>
+// #include <Foundation/NSUtilities.h>
 #include "OSUtilities.h"
+#include <Foundation/NSDate.h>
+#include <Foundation/NSProcessInfo.h>
+// #include <Foundation/NSPosixFileDescriptor.h>
+#include <Foundation/NSLock.h>
+#include <Foundation/NSByteOrder.h>
+// #include <config.h>
+#include "objc-config.h"
 
-//#if defined(HAVE_WINDOWS_H)
-//#  include <windows.h>
-//#endif
-//
-//extern NSRecursiveLock *libFoundationLock;
-//
-///* 
-// * Log a Message 
-// */
-//
-//static Class NSStringClass              = Nil;
-//static Class NSCalendarDateClass        = Nil;
-//static Class NSProcessInfoClass         = Nil;
-//
-//void NSLog(NSString *format, ...)
-//{
-//    va_list ap;
-//    
-//    va_start(ap, format);
-//    NSLogv(format, ap);
-//    va_end(ap);
-//}
-//
-//void NSLogv(NSString *format, va_list args)
-//{
-//    NSString       *message;
-//    NSCalendarDate *date;
-//    
-//    if (NSStringClass == Nil)
-//        NSStringClass = [NSString class];
-//    if (NSCalendarDateClass == Nil)
-//        NSCalendarDateClass = [NSCalendarDate class];
-//    if (NSProcessInfoClass == Nil)
-//        NSProcessInfoClass = [NSProcessInfo class];
-//    
-//    message = [[NSStringClass alloc] initWithFormat:format arguments:args];
-//    date    = [[NSCalendarDateClass alloc] init];
-//    
-//    [libFoundationLock lock];
-//    
-//    fprintf(stderr,
-//            "%s %s [%d]: %s\n",
-//            [[date descriptionWithCalendarFormat:@"%b %d %H:%M:%S"] cString],
-//            [[[NSProcessInfoClass processInfo] processName] cString],
-//#if defined(__MINGW32__)
-//            (int)GetCurrentProcessId(),
-//#else
-//            getpid(),
-//#endif
-//            [message cString]);
-//    
-//    [libFoundationLock unlock];
-//    
-//    RELEASE(date);
-//    RELEASE(message);
-//}
-//
-///* Support function for debugger */
-//NSString *_NSNewStringFromCString (const char* cString)
-//{
-//    if (NSStringClass == Nil)
-//        NSStringClass = [NSString class];
-//    return [NSStringClass stringWithCString:cString];
-//}
-//
-///* NSByteOrder.h */
-//
-//unsigned int NSHostByteOrder(void) {
-//#if WORDS_BIGENDIAN
-//    return NS_BigEndian;
-//#else
-//    return NS_LittleEndian;
-//#endif
-//}
+#if defined(HAVE_WINDOWS_H)
+#  include <windows.h>
+#endif
 
-/*
- * Convenience functions
+// extern NSRecursiveLock *libFoundationLock;
+
+/* 
+ * Log a Message 
  */
 
-LF_DECLARE unsigned __NSHashObject(void *table, const void *anObject)
-{
-    return (unsigned)[(id)anObject hash];
-}
+// static Class NSStringClass              = Nil;
+// static Class NSCalendarDateClass        = Nil;
+// static Class NSProcessInfoClass         = Nil;
 
-LF_DECLARE unsigned __NSHashPointer(void *table, const void *anObject)
-{
-    return (unsigned)((long)anObject / 4);
-}
+// void NSLog(NSString *format, ...)
+// {
+//     va_list ap;
+//     
+//     va_start(ap, format);
+//     NSLogv(format, ap);
+//     va_end(ap);
+// }
 
-LF_DECLARE BOOL __NSComparePointers(void *table,
-                    const void *anObject1,
-                    const void *anObject2)
-{
-    return anObject1 == anObject2;
-}
+// void NSLogv(NSString *format, va_list args)
+// {
+//     NSString       *message;
+//     NSCalendarDate *date;
+//     
+//     if (NSStringClass == Nil)
+//         NSStringClass = [NSString class];
+//     if (NSCalendarDateClass == Nil)
+//         NSCalendarDateClass = [NSCalendarDate class];
+//     if (NSProcessInfoClass == Nil)
+//         NSProcessInfoClass = [NSProcessInfo class];
+//     
+//     message = [[NSStringClass alloc] initWithFormat:format arguments:args];
+//     date    = [[NSCalendarDateClass alloc] init];
+//     
+//     [libFoundationLock lock];
+//     
+//     fprintf(stderr,
+//             "%s %s [%d]: %s\n",
+//             [[date descriptionWithCalendarFormat:@"%b %d %H:%M:%S"] cString],
+//             [[[NSProcessInfoClass processInfo] processName] cString],
+// #if defined(__MINGW32__)
+//             (int)GetCurrentProcessId(),
+// #else
+//             getpid(),
+// #endif
+//             [message cString]);
+//     
+//     [libFoundationLock unlock];
+//     
+//     RELEASE(date);
+//     RELEASE(message);
+// }
 
-LF_DECLARE void __NSRetainObjects(void *table, const void *anObject)
-{
-    (void)RETAIN((NSObject*)anObject);
-}
+// /* Support function for debugger */
+// NSString *_NSNewStringFromCString (const char* cString)
+// {
+//     if (NSStringClass == Nil)
+//         NSStringClass = [NSString class];
+//     return [NSStringClass stringWithCString:cString];
+// }
 
-LF_DECLARE void __NSReleaseObjects(void *table, void *anObject)
-{
-    RELEASE((NSObject*)anObject);
-}
+/* NSByteOrder.h */
 
-LF_DECLARE NSString* __NSDescribePointers(void *table, const void *anObject)
-{
-    return [NSString stringWithFormat:@"%p", anObject];
-}
+// unsigned int NSHostByteOrder(void) {
+// #if WORDS_BIGENDIAN
+//     return NS_BigEndian;
+// #else
+//     return NS_LittleEndian;
+// #endif
+// }
 
 /*
   Local Variables:
