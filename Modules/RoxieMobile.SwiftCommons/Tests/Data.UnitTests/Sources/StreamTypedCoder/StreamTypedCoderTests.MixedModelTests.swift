de1e03f480d64f6579ca5ef3d0605d2f603dac77
Natalia Mamunina <mamunina-nv@roxiemobile.com>
2019-09-03 16:20:45 +0300

    Add speed tests for MixedModel.

--- Modules/RoxieMobile.SwiftCommons/Tests/Data.UnitTests/Sources/StreamTypedCoder/StreamTypedCoderTests.MixedModelTests.swift
+++ Modules/RoxieMobile.SwiftCommons/Tests/Data.UnitTests/Sources/StreamTypedCoder/StreamTypedCoderTests.MixedModelTests.swift
@@ -35,4 +35,66 @@ class StreamTypedCoderTestsMixedModelTests: StreamTypeCoderTests
             XCTAssertEqual(_mixObject, _mixResult)
         }
     }
+
+    func testMixedModel_ToStringJSON() {
+        let _mixObject: STCMixedModel? = STCMixedModel.shared
+
+        // Positive
+        assertNoThrow {
+            // Encode
+            let jsonString = _mixObject?.toJSONString()
+
+            // Decode
+            var _mixResult: STCMixedModel?
+            if let string = jsonString {
+                _mixResult = try? STCMixedModel(from: string)
+            }
+            XCTAssertEqual(_mixObject, _mixResult)
+        }
+    }
+
+    func testSpeed_MixedModel() {
+        let _mixObject: STCMixedModel? = STCMixedModel.shared
+
+        // Positive
+        assertNoThrow {
+            var i = 0
+            while i < 1000 {
+                // Encode
+                let data = NSMutableData()
+                StreamTypedEncoder(forWritingWith: data).encodeRootObject(_mixObject)
+                XCTAssertNotEqual(data, NSMutableData())
+
+                // Decode
+                var _mixResult: STCMixedModel?
+                if let value = StreamTypedDecoder(forReadingWith: data as Data)?.decodeObject() as? STCMixedModel {
+                    _mixResult = value
+                }
+                XCTAssertEqual(_mixObject, _mixResult)
+                i += 1
+            }
+        }
+    }
+
+    func testSpeed_MixedModel_ToStringJSON() {
+        let _mixObject: STCMixedModel? = STCMixedModel.shared
+
+        // Positive
+        assertNoThrow {
+            var i = 0
+            while i < 1000 {
+                // Encode
+                let jsonString = _mixObject?.toJSONString()
+
+                // Decode
+                var _mixResult: STCMixedModel?
+                if let string = jsonString {
+                    _mixResult = try? STCMixedModel(from: string)
+                }
+
+                XCTAssertEqual(_mixObject, _mixResult)
+                i += 1
+            }
+        }
+    }
 }
