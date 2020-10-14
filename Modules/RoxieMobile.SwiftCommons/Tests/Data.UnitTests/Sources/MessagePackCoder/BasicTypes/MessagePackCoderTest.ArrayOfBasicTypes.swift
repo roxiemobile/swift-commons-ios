e348962e2e96b1fbcbc687c4ffd2dd948b1fa28c
Aleksandr Serov <Serov-ae@roxiemobile.com>
2020-10-14 19:40:11 +0300

    RXOPTS-20 Wrote MessagePackCoderTests for simple types and arrays of simple types

---
+++ Modules/RoxieMobile.SwiftCommons/Tests/Data.UnitTests/Sources/MessagePackCoder/BasicTypes/MessagePackCoderTest.ArrayOfBasicTypes.swift
@@ -0,0 +1,50 @@
+// ----------------------------------------------------------------------------
+//
+//  MessagePackCoderTest.swift
+//
+//  @author     Aleksandr Serov <serov-ae@roxiemobile.com> 
+//  @copyright  Copyright (c) 2020, Roxie Mobile Ltd. All rights reserved.
+//  @link       http://www.roxiemobile.com/
+//
+// ----------------------------------------------------------------------------
+
+import Foundation
+
+// ----------------------------------------------------------------------------
+
+class MessagePackCoderTest: XCTest
+{
+// MARK: - Construction
+
+    // ...
+
+// MARK: - Properties
+
+    // ...
+
+// MARK: - Methods
+
+    // ...
+
+// MARK: - Actions
+
+    // ...
+
+// MARK: - Private Methods
+
+    // ...
+
+// MARK: - Inner Types
+
+    // ...
+
+// MARK: - Constants
+
+    // ...
+
+// MARK: - Variables
+
+    // ...
+}
+
+// ----------------------------------------------------------------------------
