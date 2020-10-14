bb9dc82167755ee69506cbfb4f5065b50e8706e5
Aleksandr Serov <Serov-ae@roxiemobile.com>
2020-10-14 19:41:09 +0300

    RXOPTS-22 Tested MessagePackCoderTest.

--- Modules/RoxieMobile.SwiftCommons/Tests/Data.UnitTests/Sources/MessagePackCoder/BasicTypes/MessagePackCoderTest.BasicTypes.swift
+++ Modules/RoxieMobile.SwiftCommons/Tests/Data.UnitTests/Sources/MessagePackCoder/BasicTypes/MessagePackCoderTest.BasicTypes.swift
@@ -1,6 +1,6 @@
 // ----------------------------------------------------------------------------
 //
-//  MessagePackCoderTest.swift
+//  MessagePackCoderTest.BasicTypes.swift
 //
 //  @author     Aleksandr Serov <serov-ae@roxiemobile.com> 
 //  @copyright  Copyright (c) 2020, Roxie Mobile Ltd. All rights reserved.
@@ -8,34 +8,107 @@
 //
 // ----------------------------------------------------------------------------
 
-import Foundation
+@testable import SwiftCommonsData
+import SwiftCommonsLang
 import XCTest
 
 // ----------------------------------------------------------------------------
 
-class MessagePackCoderTest: XCTestCase
+extension MessagePackCoderTest
 {
-// MARK: - Tests
-
-    override func setUp() {
-        super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+    //MARK:- Tests
+    
+    func testBasicType_Boolean() {
+        
+        assertNoThrow {
+            let value: Bool = true
+            XCTAssertEqual(value, self.cloneSimpleObject(value))
+        }
+        
     }
     
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-        super.tearDown()
+    func testBasicType_SignedInteger() {
+        
+        assertNoThrow {
+            let value: Int8 = Int8.min
+            XCTAssertEqual(value, self.cloneSimpleObject(value))
+        }
+        assertNoThrow {
+            let value: Int16 = Int16.min
+            XCTAssertEqual(value, self.cloneSimpleObject(value))
+        }
+        assertNoThrow {
+            let value: Int32 = Int32.min
+            XCTAssertEqual(value, self.cloneSimpleObject(value))
+        }
+        assertNoThrow {
+            let value: Int64 = Int64.min
+            XCTAssertEqual(value, self.cloneSimpleObject(value))
+        }
+        assertNoThrow {
+            let value: Int = Int.min
+            XCTAssertEqual(value, self.cloneSimpleObject(value))
+        }
+        
     }
-
-    func testExample() {
-        // This is an example of a functional test case.
-        XCTAssert(true, "Pass")
+    
+    func testBasicType_UnsignedInteger() {
+        
+        assertNoThrow {
+            let value: UInt8 = UInt8.max
+            XCTAssertEqual(value, self.cloneSimpleObject(value))
+        }
+        assertNoThrow {
+            let value: UInt16 = UInt16.max
+            XCTAssertEqual(value, self.cloneSimpleObject(value))
+        }
+        assertNoThrow {
+            let value: UInt32 = UInt32.max
+            XCTAssertEqual(value, self.cloneSimpleObject(value))
+        }
+        assertNoThrow {
+            let value: UInt64 = UInt64.max
+            XCTAssertEqual(value, self.cloneSimpleObject(value))
+        }
+        assertNoThrow {
+            let value: UInt = UInt.max
+            XCTAssertEqual(value, self.cloneSimpleObject(value))
+        }
+        
     }
-
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measureBlock() {
-            // Put the code you want to measure the time of here.
+    
+    
+    func testBasicType_Float() {
+        
+        assertNoThrow {
+            let value: Float32 = Constants.float32Value
+            XCTAssertEqual(value, self.cloneSimpleObject(value))
+        }
+        
+        assertNoThrow {
+            let value: Float64 = Constants.float64Value
+            XCTAssertEqual(value, self.cloneSimpleObject(value))
+        }
+        
+        assertNoThrow {
+            let value: Float = Constants.floatValue
+            XCTAssertEqual(value, self.cloneSimpleObject(value))
+        }
+    }
+    
+    func testBasicType_Double() {
+        
+        assertNoThrow {
+            let value: Double = Constants.doubleValue
+            XCTAssertEqual(value, self.cloneSimpleObject(value))
+        }
+    }
+    
+    func testBasicType_String() {
+        assertNoThrow {
+            let value: String = Constants.stringValue
+            XCTAssertEqual(value, self.cloneSimpleObject(value))
+            
         }
     }
 }
