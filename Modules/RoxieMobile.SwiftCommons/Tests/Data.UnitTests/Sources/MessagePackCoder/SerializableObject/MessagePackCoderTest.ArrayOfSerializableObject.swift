6b18a5e30ebee2539ca1e3525262cd4960ba0acc
Aleksandr Serov <Serov-ae@roxiemobile.com>
2020-10-15 18:11:29 +0300

    Wrote MessagePackCoderTest for Dictionary of simple types and all Serializable object

---
+++ Modules/RoxieMobile.SwiftCommons/Tests/Data.UnitTests/Sources/MessagePackCoder/SerializableObject/MessagePackCoderTest.ArrayOfSerializableObject.swift
@@ -0,0 +1,9 @@
+//
+//  MessagePackCoderTest.ArrayOfSerializableObject.swift
+//  SwiftCommons.Data.UnitTests
+//
+//  Created by Серов Александр Евгеньевич on 15.10.2020.
+//  Copyright © 2020 Roxie Mobile Ltd. All rights reserved.
+//
+
+import Foundation
