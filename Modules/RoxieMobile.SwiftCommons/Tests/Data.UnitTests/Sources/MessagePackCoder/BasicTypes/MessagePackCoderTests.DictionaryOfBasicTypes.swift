e49873f01ff58c4d42440d53e742b602e28ee05f
Aleksandr Serov <Serov-ae@roxiemobile.com>
2020-10-20 19:52:19 +0300

    EKMBIO-4-1 Added MessagePackCoder tests files

---
+++ Modules/RoxieMobile.SwiftCommons/Tests/Data.UnitTests/Sources/MessagePackCoder/BasicTypes/MessagePackCoderTests.DictionaryOfBasicTypes.swift
@@ -0,0 +1,110 @@
+// ----------------------------------------------------------------------------
+//
+//  MessagePackCoderTests.DictionaryOfBasicTypes.swift
+//
+//  @author     Aleksandr Serov <serov-ae@roxiemobile.com>
+//  @copyright  Copyright (c) 2020, Roxie Mobile Ltd. All rights reserved.
+//  @link       http://www.roxiemobile.com/
+//
+// ----------------------------------------------------------------------------
+
+@testable import SwiftCommonsData
+import SwiftCommonsLang
+import XCTest
+
+// ----------------------------------------------------------------------------
+
+extension MessagePackCoderTests
+{
+// MARK: - Tests
+
+    func testDictionaryOf_BasicType_Boolean() {
+
+        assertNoThrow {
+            let object: [String: Bool] = [JsonKeys.value: true]
+            XCTAssertEqual(object, self.cloneSimpleObject(object))
+        }
+    }
+
+    func testDictionaryOf_BasicType_SignedInteger() {
+
+        assertNoThrow {
+            let object: [String: Int8] = [JsonKeys.value: Int8.min]
+            XCTAssertEqual(object, self.cloneSimpleObject(object))
+        }
+        assertNoThrow {
+            let object: [String: Int16] = [JsonKeys.value: Int16.min]
+            XCTAssertEqual(object, self.cloneSimpleObject(object))
+        }
+        assertNoThrow {
+            let object: [String: Int32] = [JsonKeys.value: Int32.min]
+            XCTAssertEqual(object, self.cloneSimpleObject(object))
+        }
+        assertNoThrow {
+            let object: [String: Int64] = [JsonKeys.value: Int64.min]
+            XCTAssertEqual(object, self.cloneSimpleObject(object))
+        }
+        assertNoThrow {
+            let object: [String: Int] = [JsonKeys.value: Int.min]
+            XCTAssertEqual(object, self.cloneSimpleObject(object))
+        }
+    }
+
+    func testDictionaryOf_BasicType_UnsignedInteger() {
+
+        assertNoThrow {
+            let object: [String: UInt8] = [JsonKeys.value: UInt8.max]
+            XCTAssertEqual(object, self.cloneSimpleObject(object))
+        }
+        assertNoThrow {
+            let object: [String: UInt16] = [JsonKeys.value: UInt16.max]
+            XCTAssertEqual(object, self.cloneSimpleObject(object))
+        }
+        assertNoThrow {
+            let object: [String: UInt32] = [JsonKeys.value: UInt32.max]
+            XCTAssertEqual(object, self.cloneSimpleObject(object))
+        }
+        assertNoThrow {
+            let object: [String: UInt64] = [JsonKeys.value: UInt64.max]
+            XCTAssertEqual(object, self.cloneSimpleObject(object))
+        }
+        assertNoThrow {
+            let object: [String: UInt] = [JsonKeys.value: UInt.max]
+            XCTAssertEqual(object, self.cloneSimpleObject(object))
+        }
+    }
+
+    func testDictionaryOf_BasicType_Float() {
+
+        assertNoThrow {
+            let object: [String: Float32] = [JsonKeys.value: Constants.float32Value]
+            XCTAssertEqual(object, self.cloneSimpleObject(object))
+        }
+        assertNoThrow {
+            let object: [String: Float64] = [JsonKeys.value: Constants.float64Value]
+            XCTAssertEqual(object, self.cloneSimpleObject(object))
+        }
+        assertNoThrow {
+            let object: [String: Float] = [JsonKeys.value: Constants.floatValue]
+            XCTAssertEqual(object, self.cloneSimpleObject(object))
+        }
+    }
+
+    func testDictionaryOf_BasicType_Double() {
+
+        assertNoThrow {
+            let object: [String: Double] = [JsonKeys.value: Constants.doubleValue]
+            XCTAssertEqual(object, self.cloneSimpleObject(object))
+        }
+    }
+
+    func testDictionaryOf_BasicType_String() {
+
+        assertNoThrow {
+            let object: [String: String] = [JsonKeys.value: Constants.stringValue]
+            XCTAssertEqual(object, self.cloneSimpleObject(object))
+        }
+    }
+}
+
+// ----------------------------------------------------------------------------
---
+++ Modules/RoxieMobile.SwiftCommons/Tests/Data.UnitTests/Sources/MessagePackCoder/MessagePackCoderTests.swift
@@ -0,0 +1,67 @@
+// ----------------------------------------------------------------------------
+//
+//  MessagePackCoderTests.swift
+//
+//  @author     Aleksandr Serov <serov-ae@roxiemobile.com> 
+//  @copyright  Copyright (c) 2020, Roxie Mobile Ltd. All rights reserved.
+//  @link       http://www.roxiemobile.com/
+//
+// ----------------------------------------------------------------------------
+
+@testable import SwiftCommonsData
+import SwiftCommonsConcurrent
+import XCTest
+
+// ----------------------------------------------------------------------------
+
+final class MessagePackCoderTests: XCTestCase
+{
+// MARK: - Methods
+
+    internal func assertNoThrow(action: @escaping () -> Void) -> Void {
+        var exception: NSException? = nil
+
+        objcTry {
+            action()
+        }.objcCatch { e in
+            exception = e
+        }.objcFinally {
+            XCTAssertNil(exception)
+        }
+    }
+
+    internal func assertThrowsExpection(action: @escaping () -> Void) -> Void {
+        var exception: NSException? = nil
+
+        objcTry {
+            action()
+        }.objcCatch { e in
+            exception = e
+        }.objcFinally {
+            XCTAssertNotNil(exception)
+        }
+    }
+
+    internal func cloneSimpleObject<T: Equatable>(_ object: T) -> T? {
+        let policy: CodingFailurePolicy = .raiseException
+
+        // Encode
+        let encoder = MessagePackEncoder(forWritingInto: nil, failurePolicy: policy, sortDictionaryKeys: false)
+        encoder.encode(object)
+
+        XCTAssert(encoder.error == nil)
+        XCTAssert(encoder.encodedData.isNotEmpty)
+
+        // Decode
+        let decoder = MessagePackDecoder(forReadingFrom: encoder.encodedData, failurePolicy: policy)
+        let decoderObject = decoder.decodeObject() as? T
+
+        XCTAssert(decoder.error == nil)
+        XCTAssert(decoderObject != nil)
+
+        // Done
+        return decoderObject
+    }
+}
+
+// ----------------------------------------------------------------------------
