b114a82ef7b49004e78feef041f430fc603679e4
Alexander Bragin <bragin-av@roxiemobile.com>
2021-04-19 00:44:58 +0300

    RXOPTS-92 Updated Podspec files.

--- pod_trunk_push.sh
+++ pod_trunk_push.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
-pod trunk push SwiftCommonsAbstractions.podspec
-pod trunk push SwiftCommonsObjC.podspec
-pod trunk push SwiftCommonsConcurrent.podspec
-pod trunk push SwiftCommonsLang.podspec
-pod trunk push SwiftCommonsLogging.podspec
-pod trunk push SwiftCommonsExtensions.podspec
-pod trunk push SwiftCommonsDiagnostics.podspec
-pod trunk push SwiftCommonsData.podspec
-pod trunk push SwiftCommons.podspec
+pod trunk push --synchronous SwiftCommonsAbstractions.podspec
+pod trunk push --synchronous SwiftCommonsObjC.podspec
+pod trunk push --synchronous SwiftCommonsConcurrent.podspec
+pod trunk push --synchronous SwiftCommonsLang.podspec
+pod trunk push --synchronous SwiftCommonsLogging.podspec
+pod trunk push --synchronous SwiftCommonsExtensions.podspec
+pod trunk push --synchronous SwiftCommonsDiagnostics.podspec
+pod trunk push --synchronous SwiftCommonsData.podspec
+pod trunk push --synchronous SwiftCommons.podspec
