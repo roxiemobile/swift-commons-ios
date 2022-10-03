b602b3dd0f128a3b4484c1d8a67258661d89f46f
Natalia Mamunina <mamunina-nv@roxiemobile.ru>
2022-10-03 16:28:07 +0300

    NSFPIO-1133 'CryptoSwift' updated to version 1.6.0

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.5
+// swift-tools-version: 5.6
 
 import PackageDescription
 
@@ -61,7 +61,7 @@ let package = Package(
     dependencies: [
         .package(
             url: "https://github.com/krzyzanowskim/CryptoSwift",
-            .upToNextMajor(from: "1.4.3")
+            exact: "1.6.0"
         ),
     ],
     targets: [
